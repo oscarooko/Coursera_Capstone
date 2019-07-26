ec0514335543e6bcd0776abbf6716748e6b22506
Oscar <9616131+oscarooko@users.noreply.github.com>
2019-07-26 12:41:24 -0400

    Add files via upload

---
+++ Capstone_Project.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# This notebook will be used for my Capstone Project. 
+# 
+
+# In[1]:
+
+
+# import the relevant libraries
+import pandas as pd
+import numpy as np
+
+print("Hello capstone Project Course!")
+
